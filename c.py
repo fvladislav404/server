998ffe760faeee245da7c482c7ff00ec144c5096
fvladislav404 <137600013+fvladislav404@users.noreply.github.com>
2025-09-14 17:19:41 +0300

    Update c.py

--- c.py
+++ c.py
@@ -1,9 +1,13 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 4444))
+client.connect(("localhost", 1024))
 client_name = input("Як тебе звуть")
 client.send(client_name.encode())
+while True:
+    try:
+        answer_from_servak = client.recv(1024).decode()
+        print(answer_from_servak)
+    except:
+        pass
 
-answer_from_servak = client.recv(1024).decode()
-print(answer_from_servak)
