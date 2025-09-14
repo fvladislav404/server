5f63b151992af4eb5c2e70c09d443abd9f64f75f
fvladislav404 <137600013+fvladislav404@users.noreply.github.com>
2025-09-14 17:19:25 +0300

    Update m.py2

--- m.py
+++ m.py
@@ -1,28 +1,120 @@
 import socket
-servak= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servak.bind(("localhost", 4444))
+servak = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servak.bind(("localhost", 1024))
 
+servak.setblocking(False)
 servak.listen(1)
 print("Сервер очікує підключення")
-
-info, ip_adress = servak.accept()
-print(f"До мене під'єднався кліент с ip{ip_adress}")
-
+clients = []
+while True:
+    try:
+        info, ip_adress = servak.accept()
+        print(f"До мене під'єднався кліент с ip{ip_adress}")
+        info.setblocking(False)
+        clients.append(info)
+    except:
+        pass
+for client in clients:
+    try:
+        client.send("не удалось подключиться".encode())    
+    except:
+        print("подключение")
+        clients.remove(client)
+    
 data = info.recv(1024).decode()
-print(data)
+
 
 info.send(f"Привіт {data}".encode())
 
 info.close()
 servak.close()import socket
-servak= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servak.bind(("localhost", 4444))
+servak = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servak.bind(("localhost", 1024))
 
+servak.setblocking(False)
 servak.listen(1)
 print("Сервер очікує підключення")
+clients = []
+while True:
+    try:
+        info, ip_adress = servak.accept()
+        print(f"До мене під'єднався кліент с ip{ip_adress}")
+        info.setblocking(False)
+        clients.append(info)
+    except:
+        pass
+for client in clients:
+    try:
+        client.send("не удалось подключиться".encode())    
+    except:
+        print("подключение")
+        clients.remove(client)
+    
+data = info.recv(1024).decode()
 
-info, ip_adress = servak.accept()
-print(f"До мене під'єднався кліент с ip{ip_adress}")
+
+info.send(f"Привіт {data}".encode())
+
+info.close()
+servak.close()import socket
+servak = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servak.bind(("localhost", 1024))
+
+servak.setblocking(False)
+servak.listen(1)
+print("Сервер очікує підключення")
+clients = []
+while True:
+    try:
+        info, ip_adress = servak.accept()
+        print(f"До мене під'єднався кліент с ip{ip_adress}")
+        info.setblocking(False)
+        clients.append(info)
+    except:
+        pass
+for client in clients:
+    try:
+        client.send("не удалось подключиться".encode())    
+    except:
+        print("подключение")
+        clients.remove(client)
+    
+data = info.recv(1024).decode()
+
+
+info.send(f"Привіт {data}".encode())
+
+info.close()
+servak.close()import socket
+servak = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servak.bind(("localhost", 1024))
+
+servak.setblocking(False)
+servak.listen(1)
+print("Сервер очікує підключення")
+clients = []
+while True:
+    try:
+        info, ip_adress = servak.accept()
+        print(f"До мене під'єднався кліент с ip{ip_adress}")
+        info.setblocking(False)
+        clients.append(info)
+    except:
+        pass
+for client in clients:
+    try:
+        client.send("не удалось подключиться".encode())    
+    except:
+        print("подключение")
+        clients.remove(client)
+    
+data = info.recv(1024).decode()
+
+
+info.send(f"Привіт {data}".encode())
+
+info.close()
+servak.close()
 
 data = info.recv(1024).decode()
 print(data)
@@ -47,3 +139,4 @@ info.send(f"Привіт {data}".encode())
 
 info.close()
 servak.close()
+
